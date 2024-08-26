ca713b3bb2b0a9fb588729a6f9969b96c7bceedb
杨凯伟 <21376218@buaa.edu.cn>
2024-08-26 10:43:38 +0800

    all can run

--- novel-core/novel-common/src/main/java/io/github/xxyopen/novel/common/auth/JwtUtils.java
+++ novel-core/novel-common/src/main/java/io/github/xxyopen/novel/common/auth/JwtUtils.java
@@ -7,10 +7,6 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.stereotype.Component;
-
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
